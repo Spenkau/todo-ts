aa8132005003cc57aed9ad604e1644495ca243cd
Spenkau <96494361+Spenkau@users.noreply.github.com>
2023-05-17 19:41:24 +0300

    *

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {lazy, Suspense, useEffect, useRef, useState} from 'react';
+import List from "./components/List";
+import {ITodo} from "./components/data";
+import "./styles.css"
+
+const storage: ITodo[] =  JSON.parse(localStorage.getItem('todos')!)
+
+
 
 function App() {
+    // const [state, dispatch] = useReducer(reducer, state)
+    const [showModal, setShowModal] = useState<boolean>(false)
+    const [todos, setTodos] = useState<ITodo[]>(storage ? storage : [])
+    const test = useRef(null)
+
+    const AddTodoModal = lazy(() => import("./components/AddTodoModal"))
+
+    function modalInteract(): void {
+        setShowModal(!showModal)
+    }
+
+    useEffect(() => {
+        const todoList = localStorage.getItem('todos')
+        if (todoList !== null) setTodos(JSON.parse(todoList))
+    }, [])
+
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos))
+    },[todos])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className={"todo-container"} ref={test}>
+          <h1 className={"todo-header"}>TODO-Лист</h1>
+          <button className={"modal-interact"} onClick={modalInteract} >
+              <p style={{rotate: showModal ? "45deg" : "0deg"}}>+</p>
+          </button>
+          {
+              showModal ?
+                  <Suspense fallback={<div>Loading...</div>}>
+                      <AddTodoModal
+                          todos={todos}
+                          setTodos={setTodos}
+                          setShowModal={setShowModal}
+                      />
+                  </Suspense>
+              : null
+          }
+          {<List todos={todos} setTodos={setTodos}  />}
+      </div>
   );
 }
 
